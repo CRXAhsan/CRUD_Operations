06248d8c743594774996bfffb2f93760ad3ca31d
CRXAhsan <ahsantariq669@gmail.com>
2023-08-20 22:41:59 +0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace CRUD_Operations
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_name = new System.Windows.Forms.Label();
+            this.lbl_roll_no = new System.Windows.Forms.Label();
+            this.txt_Name = new System.Windows.Forms.TextBox();
+            this.txt_rollNo = new System.Windows.Forms.TextBox();
+            this.btn_view = new System.Windows.Forms.Button();
+            this.btn_Insert = new System.Windows.Forms.Button();
+            this.btn_Update = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Teal;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(209, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(331, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student Information System";
+            // 
+            // lbl_name
+            // 
+            this.lbl_name.AutoSize = true;
+            this.lbl_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_name.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_name.Location = new System.Drawing.Point(134, 108);
+            this.lbl_name.Name = "lbl_name";
+            this.lbl_name.Size = new System.Drawing.Size(60, 20);
+            this.lbl_name.TabIndex = 1;
+            this.lbl_name.Text = "Name:";
+            // 
+            // lbl_roll_no
+            // 
+            this.lbl_roll_no.AutoSize = true;
+            this.lbl_roll_no.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_roll_no.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_roll_no.Location = new System.Drawing.Point(117, 163);
+            this.lbl_roll_no.Name = "lbl_roll_no";
+            this.lbl_roll_no.Size = new System.Drawing.Size(77, 20);
+            this.lbl_roll_no.TabIndex = 2;
+            this.lbl_roll_no.Text = "Roll_No:";
+            // 
+            // txt_Name
+            // 
+            this.txt_Name.Location = new System.Drawing.Point(253, 107);
+            this.txt_Name.Name = "txt_Name";
+            this.txt_Name.Size = new System.Drawing.Size(191, 20);
+            this.txt_Name.TabIndex = 3;
+            // 
+            // txt_rollNo
+            // 
+            this.txt_rollNo.Location = new System.Drawing.Point(253, 165);
+            this.txt_rollNo.Name = "txt_rollNo";
+            this.txt_rollNo.Size = new System.Drawing.Size(191, 20);
+            this.txt_rollNo.TabIndex = 4;
+            // 
+            // btn_view
+            // 
+            this.btn_view.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_view.ForeColor = System.Drawing.Color.Teal;
+            this.btn_view.Location = new System.Drawing.Point(121, 261);
+            this.btn_view.Name = "btn_view";
+            this.btn_view.Size = new System.Drawing.Size(73, 38);
+            this.btn_view.TabIndex = 5;
+            this.btn_view.Text = "View";
+            this.btn_view.UseVisualStyleBackColor = true;
+            this.btn_view.Click += new System.EventHandler(this.btn_view_Click);
+            // 
+            // btn_Insert
+            // 
+            this.btn_Insert.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Insert.ForeColor = System.Drawing.Color.Teal;
+            this.btn_Insert.Location = new System.Drawing.Point(253, 261);
+            this.btn_Insert.Name = "btn_Insert";
+            this.btn_Insert.Size = new System.Drawing.Size(81, 38);
+            this.btn_Insert.TabIndex = 5;
+            this.btn_Insert.Text = "Insert";
+            this.btn_Insert.UseVisualStyleBackColor = true;
+            this.btn_Insert.Click += new System.EventHandler(this.btn_Insert_Click);
+            // 
+            // btn_Update
+            // 
+            this.btn_Update.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Update.ForeColor = System.Drawing.Color.Teal;
+            this.btn_Update.Location = new System.Drawing.Point(383, 261);
+            this.btn_Update.Name = "btn_Update";
+            this.btn_Update.Size = new System.Drawing.Size(81, 38);
+            this.btn_Update.TabIndex = 5;
+            this.btn_Update.Text = "Update";
+            this.btn_Update.UseVisualStyleBackColor = true;
+            this.btn_Update.Click += new System.EventHandler(this.btn_Update_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_delete.ForeColor = System.Drawing.Color.Teal;
+            this.btn_delete.Location = new System.Drawing.Point(519, 261);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(81, 38);
+            this.btn_delete.TabIndex = 5;
+            this.btn_delete.Text = "Delete";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(800, 455);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_Update);
+            this.Controls.Add(this.btn_Insert);
+            this.Controls.Add(this.btn_view);
+            this.Controls.Add(this.txt_rollNo);
+            this.Controls.Add(this.txt_Name);
+            this.Controls.Add(this.lbl_roll_no);
+            this.Controls.Add(this.lbl_name);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_name;
+        private System.Windows.Forms.Label lbl_roll_no;
+        private System.Windows.Forms.TextBox txt_Name;
+        private System.Windows.Forms.TextBox txt_rollNo;
+        private System.Windows.Forms.Button btn_view;
+        private System.Windows.Forms.Button btn_Insert;
+        private System.Windows.Forms.Button btn_Update;
+        private System.Windows.Forms.Button btn_delete;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Runtime.Remoting.Contexts;
+using System.Web;
+using System.IO;
+
+namespace CRUD_Operations
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_view_Click(object sender, EventArgs e)
+        {
+            string file = "D:\\Repository\\CRUD_Operations\\StudentsFile.txt";
+            string readText =File.ReadAllText(file);
+            if(readText != "")
+            {
+                MessageBox.Show("Student Found:\n " + readText, "Found");
+            }
+            else
+            {
+                MessageBox.Show("No Records Found:\n ", "Found");
+            }
+        }
+
+        private void btn_Insert_Click(object sender, EventArgs e)
+        {
+            string file = "D:\\Repository\\CRUD_Operations\\StudentsFile.txt";
+            string name = txt_Name.Text;
+            string rollNo = txt_rollNo.Text;
+
+            using(StreamWriter writer = new StreamWriter(file,true))
+            {
+                writer.WriteLine(name + "," + rollNo);//John,123
+            }
+
+            MessageBox.Show("Student Data\n Name: " + name + "\nRoll No: " + rollNo + "\nInserted Successfully...", "Student Data", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void btn_Update_Click(object sender, EventArgs e)
+        {
+            string file = "D:\\Repository\\CRUD_Operations\\StudentsFile.txt";
+            string name = txt_Name.Text;
+            string rollNo = txt_rollNo.Text;
+
+            if(File.Exists(file))
+            {
+                var lines = File.ReadAllLines(file);
+                var LineToUpdate = lines.FirstOrDefault(Line => Line.StartsWith(Name + ","));
+
+                if(LineToUpdate != null)
+                {
+                    var UpdatedLine = name + "," + rollNo;
+
+                    lines[Array.IndexOf(lines, UpdatedLine)] = UpdatedLine;
+
+                    File.WriteAllLines(file, lines);
+                    MessageBox.Show("Student Record Updated Successfully...", "Update Record");
+                }
+                else
+                {
+                    MessageBox.Show("Student Not Found...", "Not Found");
+                }
+            }
+            else
+            {
+                MessageBox.Show("File Not Exist...", "Error");
+            }
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            string file = "D:\\Repository\\CRUD_Operations\\StudentsFile.txt";
+
+            if (File.Exists(file))
+            {
+                if(file != null)
+                {
+                    File.WriteAllText(file, string.Empty);
+                    MessageBox.Show("File Contents Deleted Successfully...", "Deletion Succesfull");
+                }
+                else
+                {
+                    MessageBox.Show("File is Already Empty...", "Already Empty");
+                }
+            }
+            else
+            {
+                MessageBox.Show("File Not Exist...", "Error");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
